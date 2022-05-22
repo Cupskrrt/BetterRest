999d0d725095267a6616ca76aebdab447f812315
Cupang5 <khanalfarizzi@hotmail.com>
2022-05-22 18:45:58 +0700

    Learning core ml

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -4,13 +4,83 @@
 //
 //  Created by Luthfi Khan on 22/05/22.
 //
-
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
+    @State private var wakeUp = defaultWakeTime
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMsg = ""
+    @State private var alertShow = false
+    
+    static var defaultWakeTime: Date{
+        var components =  DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date.now
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            Form {
+                VStack(alignment: .leading, spacing: 0){
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                    
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                }
+                
+                VStack(alignment: .leading, spacing: 0){
+                    Text("Amount of sleep")
+                        .font(.headline)
+                    
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                }
+                
+                VStack(alignment: .leading, spacing: 0){
+                    Text("Daily cup(s) of coffee")
+                        .font(.headline)
+                    
+                    Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                }
+            }
+            .navigationTitle("BetterRest")
+            .toolbar{
+                Button("Calculate", action: calculateBedtime)
+            }
+            .alert(alertTitle, isPresented: $alertShow){
+                Button("OK"){}
+            } message: {
+                Text(alertMsg)
+            }
+        }
+    }
+    
+    func calculateBedtime(){
+        //buat masukin mlmodelnya
+        do{
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let component = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (component.hour ?? 0) * 60 * 60
+            let minute = (component.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            alertTitle = "Your ideal bedtime is.."
+            alertMsg = sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            alertTitle = "Error"
+            alertMsg = "Sorry, there was a problem calculating your bedtime."
+        }
+        
+        alertShow  = true
     }
 }
 
