f10ac5d851f42d223127b6b806c3811d03f7f9b7
Cupang5 <khanalfarizzi@hotmail.com>
2022-05-22 18:09:17 +0700

    Initial Commit

---
+++ Shared/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  Shared
+//
+//  Created by Luthfi Khan on 22/05/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
